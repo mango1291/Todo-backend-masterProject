fe08440cdecac6fe816e1f764b2f7d9dfd499b37
Mangesh.Pawar <Mangesh.Pawar@MININT-EIPMN5F.st.com>
2024-01-25 18:49:56 +0530

    Initial Commit

---
+++ Todo-backend-Java/todo-backend-masterProject/src/main/java/com/jcs/todomvc/Application.java
@@ -0,0 +1,16 @@
+package com.jcs.todomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+//@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
